1be3c3f28f817390a220fa955514d459eae9ee6f
kai gonzalez <gkai70264@gmail.com>
2021-11-15 20:03:30 -0500

    Initial Commit

---
+++ Nothing/Program.cs
@@ -0,0 +1,5 @@
+﻿// Nothing!
+// This shell begins with nothing as a starting point.
+// You can add libs, features, etc ...
+
+// all in a push!
---
+++ README.md
@@ -0,0 +1,5 @@
+# Nothing
+
+Literally Nothing.
+
+You can add on to it by forking it and changing it. Thanks!
